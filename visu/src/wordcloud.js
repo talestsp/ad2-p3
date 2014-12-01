38fa2feb088f3f69bbd6db19867c66519964292b
talestsp <tales.tsp@gmail.com>
2014-11-30 23:33:26 -0300

    Visualização da wordcloud pronta

--- visu/src/wordcloud.js
+++ visu/src/wordcloud.js
@@ -1,14 +1,38 @@
-var fontSize = d3.scale.log().range([10, 100]);
+function desenharWCloud(words) {
+         var fill = d3.scale.category20();
 
-var layout = cloud()
-    .size([960, 600])
-    .timeInterval(10)
-    .text(function(d) { return d.key; })
-    .font("Impact")
-    .fontSize(function(d) { return fontSize(+d.value); })
-    .rotate(function(d) { return ~~(Math.random() * 5) * 30 - 60; })
-    .padding(1)
-    .on("word", progress)
-    .on("end", draw)
-    .words(["aaa", "aaa", "aaa", "aaa", "aaa", "aaa", "aaa", "aaa", "bbbbbb", "bbbbbb", "bbbbbb", "bbbbbb", "bbbbbb"])
-    .start();
+          d3.layout.cloud().size([400, 300])
+          .words(words.map(function(d) {
+            return {text: d, size: 10 + Math.random() * 90};
+          }))
+          .padding(5)
+          .rotate(function() { return ~~(Math.random() * 2) * 90; })
+          .font("Impact")
+          .fontSize(function(d) { return d.size; })
+          .on("end", draw)
+          .start();
+
+
+//limpar a div #wcloud
+        function draw(words) {
+        d3.select("#cloud_id").remove();
+
+        d3.select("#wcloud").append("svg")
+            .attr("id", "cloud_id")
+            .attr("width", 400)
+            .attr("height", 400)
+          .append("g")
+            .attr("transform", "translate(200,150)")
+          .selectAll("text")
+            .data(words)
+          .enter().append("text")
+            .style("font-size", function(d) { return d.size + "px"; })
+            .style("font-family", "Impact")
+            .style("fill", function(d, i) { return fill(i); })
+            .attr("text-anchor", "middle")
+            .attr("transform", function(d) {
+              return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function(d) { return d.text; });
+  }
+  }
